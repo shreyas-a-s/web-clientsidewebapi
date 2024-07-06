492fa61263a87b64a94fb0c10042d83131dabbf6
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-07 01:25:53 +0530

    feat: Add nameDisplayCheck function that integrates prev functions

--- index.js
+++ index.js
@@ -27,3 +27,25 @@ forgetBtn.addEventListener("click", () => {
   // run nameDisplayCheck() to sort out displaying the generic greeting again and updating the form display
   nameDisplayCheck();
 });
+
+// define the nameDisplayCheck() function
+function nameDisplayCheck() {
+  // check whether the 'name' data item is stored in web Storage
+  if (localStorage.getItem("name")) {
+    // If it is, display personalized greeting
+    const name = localStorage.getItem("name");
+    h1.textContent = `Welcome, ${name}`;
+    personalGreeting.textContent = `Welcome to our website, ${name}! We hope you have fun while you are here.`;
+    // hide the 'remember' part of the form and show the 'forget' part
+    forgetDiv.style.display = "block";
+    rememberDiv.style.display = "none";
+  } else {
+    // if not, display generic greeting
+    h1.textContent = "Welcome to our website ";
+    personalGreeting.textContent =
+      "Welcome to our website. We hope you have fun while you are here.";
+    // hide the 'forget' part of the form and show the 'remember' part
+    forgetDiv.style.display = "none";
+    rememberDiv.style.display = "block";
+  }
+}
