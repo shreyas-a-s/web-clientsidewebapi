b1205477ca67fda3a8ed8f7faa811375b3db9a28
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-07 01:23:47 +0530

    feat: Add function that needs to run when 'Say hello' button is clicked

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ const personalGreeting = document.querySelector(".personal-greeting");
 
 // Stop the form from submitting when a button is pressed
 form.addEventListener("submit", (e) => e.preventDefault());
+
+// run function when the 'Say hello' button is clicked
+submitBtn.addEventListener("click", () => {
+  // store the entered name in web storage
+  localStorage.setItem("name", nameInput.value);
+  // run nameDisplayCheck() to sort out displaying the personalized greetings and updating the form display
+  nameDisplayCheck();
+});
